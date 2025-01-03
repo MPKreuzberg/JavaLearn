a065d576b15bdf230c4568da624ded2fd800176c
MPKreuzberg <fabrisigor01@gmail.com>
2025-01-03 22:49:17 +0100

    hello world changed

--- java_1441_book_learn/src/helloWorld/Main.java
+++ java_1441_book_learn/src/helloWorld/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello from MPKreuzberg :-)");
+		System.out.println("My first text written on Dvorak keyboard");
 		
 	}
 
