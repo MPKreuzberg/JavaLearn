4109909f6611ac896fcd764d4e2a2aeda05b6d54
MPKreuzberg <fabrisigor01@gmail.com>
2025-01-03 13:49:55 +0100

    unary operators added

---
+++ java_1441_book_learn/src/unaryOperators/Main.java
@@ -0,0 +1,34 @@
+package unaryOperators;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int x = 3;
+		++x;
+
+		System.out.println(x);
+		System.out.println(x++);
+		System.out.println(x);
+
+		int y = 4;
+		y--;
+		System.out.println(y);
+		System.out.println(--y);
+		System.out.println(y);
+
+//		int x = 6;
+//		int y = -x;
+//		System.out.println(x);
+//		System.out.println(y);
+//
+//		double z = (double) 3.45;
+//		System.out.println(z);
+//
+//		boolean b = true;
+//		System.out.println(!b);
+//		System.out.println(b);
+
+	}
+
+}
